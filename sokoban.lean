1218fecbf568669ac123256d430a151a900f67b3
mirefek <mirek@olsak.net>
2021-03-19 22:47:48 +0100

    Add files via upload

--- sokoban.lean
+++ sokoban.lean
@@ -66,7 +66,7 @@ open list2d
 instance : inhabited sokoban
 := ⟨{ available := [[tt]], boxes := [], storages := [], storekeeper := (0,0) }⟩
 
-@[simp] def move (d : direction) (s : sokoban) : sokoban :=
+def move (d : direction) (s : sokoban) : sokoban :=
   match (d.shift s.storekeeper) with none := s
   | some sk2 :=
   if get2d sk2 s.available = false then s else
@@ -84,18 +84,18 @@ instance : inhabited sokoban
     end
   end
 
-@[simp] def move_up := move direction.up
-@[simp] def move_down := move direction.down
-@[simp] def move_left := move direction.left
-@[simp] def move_right := move direction.right
+def move_up := move direction.up
+def move_down := move direction.down
+def move_left := move direction.left
+def move_right := move direction.right
 
-@[simp] def add_newline (s : sokoban) : sokoban := {
+def add_newline (s : sokoban) : sokoban := {
   available := []::s.available,
   boxes := []::s.boxes,
   storages := []::s.storages,
   storekeeper := match s.storekeeper with (x,y) := (x,y+1) end
 }
-@[simp] def add_newsquare (av box stor sk : bool) (s : sokoban) : sokoban := {
+def add_newsquare (av box stor sk : bool) (s : sokoban) : sokoban := {
   available := add_to_line1 av s.available,
   boxes := add_to_line1 box s.boxes,
   storages := add_to_line1 stor s.storages,
@@ -105,7 +105,7 @@ instance : inhabited sokoban
   end
 }
 
-@[simp] def from_string_aux : list char → option (sokoban × bool)
+def from_string_aux : list char → option (sokoban × bool)
 | [] := some (sokoban.mk [] [] [] (0,0), ff)
 | (c::str) := match (from_string_aux str), c with
   | none, _ := none
@@ -120,14 +120,14 @@ instance : inhabited sokoban
   | (some _), _ := none
   end
 
-@[simp] def from_string (str : string) : sokoban :=
+def from_string (str : string) : sokoban :=
   match (from_string_aux str.to_list) with
   | none := default sokoban
   | some (_, ff) := default sokoban
   | some (level, tt) := level
   end
 
-@[simp] def square_to_char : bool → bool → bool → bool → char
+def square_to_char : bool → bool → bool → bool → char
 | tt ff ff ff := ' '
 | ff ff ff ff := '#'
 | tt ff tt ff := '.'
@@ -137,11 +137,11 @@ instance : inhabited sokoban
 | tt ff tt tt := '+'
 | _ _ _ _ := '?'
 
-@[simp] def to_string_aux1 (str : list char) : list (bool × bool × bool × bool) → list char
+def to_string_aux1 (str : list char) : list (bool × bool × bool × bool) → list char
 | [] := str
 | ((av,box,stor,sk)::t) := (square_to_char av box stor sk)::(to_string_aux1 t)
 
-@[simp] def to_string_aux2 : list2d (bool × bool × bool × bool) → list char
+def to_string_aux2 : list2d (bool × bool × bool × bool) → list char
 | [] := []
 | (h::t) := to_string_aux1 ('\n'::(to_string_aux2 t)) h
 
@@ -157,10 +157,42 @@ inductive solvable : sokoban -> Prop
 | solved (s : sokoban) (H : s.boxes = s.storages) : solvable s
 | move (d : direction) (s : sokoban) (H : solvable (s.move d)) : solvable s
 
-@[simp] def solvable.move_up := solvable.move direction.up
-@[simp] def solvable.move_down := solvable.move direction.down
-@[simp] def solvable.move_left := solvable.move direction.left
-@[simp] def solvable.move_right := solvable.move direction.right
+def solvable.move_up := solvable.move direction.up
+def solvable.move_down := solvable.move direction.down
+def solvable.move_left := solvable.move direction.left
+def solvable.move_right := solvable.move direction.right
+
+meta def soko_simp_root (e : expr) : tactic unit :=
+do
+  soko ← tactic.eval_expr sokoban e,
+  tactic.trace (to_string soko),
+  let p : pexpr := ``(%%e = sokoban.mk
+    %%soko.available %%soko.boxes %%soko.storages %%soko.storekeeper),
+  eq ← tactic.to_expr p,
+  name ← tactic.get_unused_name,
+  H ← tactic.assert name eq,
+  tactic.reflexivity,
+  tactic.rewrite_target H,
+  tactic.clear H
+
+meta def soko_simp_expr : expr → tactic unit := assume e : expr,
+soko_simp_root e <|> match e with
+| expr.app e1 e2 := soko_simp_expr e1 >> soko_simp_expr e2
+| _ := return ()
+end
+
+meta def soko_simp : tactic unit :=
+do
+  goal ← tactic.target,
+  soko_simp_expr goal
+
+#check tactic.apply
+
+meta def solve_up : tactic unit := tactic.apply `(solvable.move_up) >> soko_simp
+meta def solve_down : tactic unit := tactic.apply `(solvable.move_down) >> soko_simp
+meta def solve_left : tactic unit := tactic.apply `(solvable.move_left) >> soko_simp
+meta def solve_right : tactic unit := tactic.apply `(solvable.move_right) >> soko_simp
+meta def solve_finish : tactic unit := tactic.apply `(sokoban.solvable.solved) >> tactic.reflexivity
 
 end sokoban
 
@@ -173,93 +205,66 @@ def soko_level := sokoban.from_string "
 #. . .#
 #######
 "
-/-
-def soko_level_decoded : sokoban :=
-{available := [list.nil, [ff, ff, ff, ff, ff, ff, ff], [ff, tt, tt, tt, tt, tt, ff], [ff, tt, tt, tt, tt, tt, ff], [ff, tt, tt, tt, tt, tt, ff], [ff, tt, tt, tt, tt, tt, ff], [ff, tt, tt, tt, tt, tt, ff], [ff, ff, ff, ff, ff, ff, ff]],
- boxes := [list.nil, [ff, ff, ff, ff, ff, ff, ff], [ff, ff, ff, ff, ff, ff, ff], [ff, ff, tt, tt, tt, ff, ff], [ff, ff, tt, ff, tt, ff, ff], [ff, ff, tt, tt, tt, ff, ff], [ff, ff, ff, ff, ff, ff, ff], [ff, ff, ff, ff, ff, ff, ff]],
- storages := [list.nil, [ff, ff, ff, ff, ff, ff, ff], [ff, tt, ff, tt, ff, tt, ff], [ff, ff, ff, ff, ff, ff, ff], [ff, tt, ff, ff, ff, tt, ff], [ff, ff, ff, ff, ff, ff, ff], [ff, tt, ff, tt, ff, tt, ff], [ff, ff, ff, ff, ff, ff, ff]],
- storekeeper := (3, 4)}
--/
 
-example : sokoban.solvable soko_level :=
+example : soko_level.solvable :=
 begin
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_right,
-/-
-#######
-#. * .#
-#$   $#
-#.$ $.#
-#$  @$#
-#. * .#
-#######
--/
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_right,
-/-
-#######
-#. * *#
-#   $ #
-#*  @.#
-#  $ $#
-#* * .#
-#######
--/
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_right,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_left,
-  apply sokoban.solvable.move_down,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.move_up,
-  apply sokoban.solvable.solved,
-  reflexivity
+  sokoban.soko_simp,
+  sokoban.solve_up,
+  sokoban.solve_left,
+  sokoban.solve_right,
+  sokoban.solve_right,
+  sokoban.solve_left,
+  sokoban.solve_down,
+  sokoban.solve_down,
+  sokoban.solve_left,
+  sokoban.solve_right,
+  sokoban.solve_right,
+  sokoban.solve_up,
+  sokoban.solve_right,
+  sokoban.solve_up,
+  sokoban.solve_down,
+  sokoban.solve_left,
+  sokoban.solve_left,
+  sokoban.solve_up,
+  sokoban.solve_left,
+  sokoban.solve_down,
+  sokoban.solve_left,
+  sokoban.solve_down,
+  sokoban.solve_right,
+  sokoban.solve_up,
+  sokoban.solve_up,
+  sokoban.solve_up,
+  sokoban.solve_left,
+  sokoban.solve_down,
+  sokoban.solve_right,
+  sokoban.solve_down,
+  sokoban.solve_right,
+  sokoban.solve_right,
+  sokoban.solve_right,
+  sokoban.solve_up,
+  sokoban.solve_left,
+  sokoban.solve_down,
+  sokoban.solve_down,
+  sokoban.solve_down,
+  sokoban.solve_right,
+  sokoban.solve_up,
+  sokoban.solve_left,
+  sokoban.solve_up,
+  sokoban.solve_up,
+  sokoban.solve_up,
+  sokoban.solve_left,
+  sokoban.solve_left,
+  sokoban.solve_down,
+  sokoban.solve_down,
+  sokoban.solve_down,
+  sokoban.solve_down,
+  sokoban.solve_right,
+  sokoban.solve_right,
+  sokoban.solve_up,
+  sokoban.solve_up,
+  sokoban.solve_left,
+  sokoban.solve_down,
+  sokoban.solve_up,
+  sokoban.solve_up,
+  sokoban.solve_finish
 end
-
-#eval soko_level.move_up.move_left.move_right.move_right.move_left.move_down.move_down.move_left.move_right.move_right.move_up.move_right.move_up.move_down.move_left.move_left.move_up.move_left.move_down.move_left.move_down.move_right.move_up.move_up.move_up.move_left.move_down.move_right.move_down.move_right.move_right.move_right.move_up.move_left.move_down.move_down.move_down.move_right.move_up.move_left.move_up.move_up.move_up.move_left.move_left.move_down.move_down.move_down.move_down.move_right.move_right.move_up.move_up.move_left.move_down.move_up.move_up
